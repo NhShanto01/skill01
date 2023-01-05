b379bdbd65ab5e0be3a1ec19222dc3b93182f871
NhShanto01 <mdnurhossain940@gmail.com>
2023-01-05 10:37:31 +0600

    Home part updated

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ChooseSkill from './ChooseSkill';
+import Commertial from './Commertial';
+import EMI from './EMI';
 import Hero from './Hero';
+import Residential from './Residential';
+import Services from './Services';
 
 const Home = () => {
     return (
@@ -8,6 +12,16 @@ const Home = () => {
             <Hero></Hero>
             <p className='mt-3 fs-4 fw-bold'>Why Choose Skill?</p>
             <ChooseSkill></ChooseSkill>
+            <p className='mt-3 fs-4 fw-bold'>Stay Safe & Design Virtually</p>
+
+            <Services></Services>
+            <hr className="text-dark"></hr>
+            <p className='my-4 fs-4 fw-bold'>Residential</p>
+            <Residential></Residential>
+            <hr className="text-dark"></hr>
+            <p className='my-4 fs-4 fw-bold'>Commertial</p>
+            <Commertial></Commertial>
+            <EMI></EMI>
         </div>
     );
 };
