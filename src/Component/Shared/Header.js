f10f06575d5589aeba4deca90e686494b77f2b7d
NhShanto01 <mdnurhossain940@gmail.com>
2023-01-05 10:45:58 +0600

    updated header

--- src/Component/Shared/Header.js
+++ src/Component/Shared/Header.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Container, Navbar, Nav} from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import logo from'../Image/logo.svg';
 
 const Header = () => {
     return (
-        <Navbar bg="dark" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mx-end fw-bold">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#service">Service</Nav.Link>
-            <Nav.Link href="#design">Design</Nav.Link>
-            <Nav.Link href="#shop">Shop</Nav.Link>
-            <Nav.Link href="#aboutus">About Us</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+        <Navbar bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="#home">
+                  <img src={logo} />
+                </Navbar.Brand>
+                <Nav className="mx-end fw-bold">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#service">Service</Nav.Link>
+                    <Nav.Link href="#design">Design</Nav.Link>
+                    <Nav.Link href="#shop">Shop</Nav.Link>
+                    <Nav.Link href="#aboutus">About Us</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 };
 
