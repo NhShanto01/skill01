1eba89ad5e8dee14870e493bdbb806d708bae516
NhShanto01 <mdnurhossain940@gmail.com>
2023-01-06 01:35:13 +0600

    updated Pays Friend

--- src/Component/Home/PaysFriend.js
+++ src/Component/Home/PaysFriend.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import paysf from '../Image/i_it pays to have friend/Group 273.svg';
+import paysfri from '../Image/i_it pays to have friend/Group 273.svg';
 import './PaysFriend.css';
 
 const PaysFriend = () => (
-    <div className='pays-color d-flex'>
+    <div className='pays-color d-flex justify-content-center mt-5 py-5'>
         <div>
-            <img src={paysf} />
+            <img className='pays-img' src={paysfri} alt="" />
+        </div>
+        <div className='ms-5'>
+            <p className='fs-2'>It Pays to have friend</p>
+            <p>Earn money every time your friend try Skill Interior</p>
+            <button className='hero-btn'>Refar Now</button>
         </div>
 
     </div>
